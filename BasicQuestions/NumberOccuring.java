88c531380eeae5378b4c9179492bbd813351dd41
ummarFurquan <mohammedummar29@gmail.com>
2024-12-11 18:45:36 +0530

    Correct Code

--- BasicQuestions/NumberOccuring.java
+++ BasicQuestions/NumberOccuring.java
@@ -1,6 +1,5 @@
 package BasicQuestions;
 
-import java.util.Scanner;
 
 public class NumberOccuring {
     public static void main(String[] args) {
@@ -8,10 +7,9 @@ public class NumberOccuring {
         int a  = 5;
         int count = 0;
         while(n > 0){
-            int rem = 0;
 
-            rem = n % 10;
-            if(n == a){
+           int  rem = n % 10;
+            if(rem == a){
                 count++;
             }
                 n = n / 10;
