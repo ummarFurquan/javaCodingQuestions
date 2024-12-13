fcde834b19f84b2b6c720e0c5f0efc544eab4148
ummarFurquan <mohammedummar29@gmail.com>
2024-12-14 00:26:24 +0530

    Function and Method Example

--- BasicQuestions/Greeting.java
+++ BasicQuestions/Greeting.java
@@ -1,13 +1,18 @@
 package BasicQuestions;
 
 
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
+        Scanner in = new Scanner(System.in);
+        String message = in.next();
+        String greeting = greet(message); //Storing the Method in a String data type and passing arguments
+        System.out.print(greeting);
+
     }
 
-   private  static String greet(){
-       return "Hello";
+   static  String greet(String  name){    // greet method that accepts String data type as a parameter and returns a String
+       return "Hello " + name;
     }
 }
