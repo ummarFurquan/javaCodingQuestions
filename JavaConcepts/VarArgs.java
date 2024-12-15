8861d71d2e2a1c530c3b6cdb2d319ba86ea6e288
ummarFurquan <mohammedummar29@gmail.com>
2024-12-15 13:04:46 +0530

    Variable Arguments

---
+++ JavaConcepts/VarArgs.java
@@ -0,0 +1,19 @@
+package JavaConcepts;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        // i can pass any number of arguments
+        int response =  sample(1,2,3,4,5,6);
+        System.out.print(response);
+    }
+    static int  sample(int ...n){ //can accept any number of arguments and store it like a array of Integers
+        System.out.println(Arrays.toString(n));
+        int sum = 0;
+        for(int i = 0; i<n.length; i++){
+            sum = sum + n[i]; // Return the sum of array of integers
+        }
+        return sum;
+    }
+}
