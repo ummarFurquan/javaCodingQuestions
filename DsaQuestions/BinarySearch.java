e7e0c353041322cb5f112a9efa7ad5fc76643f9e
ummarFurquan <mohammedummar29@gmail.com>
2024-12-20 19:54:27 +0530

    Binary Search Questions

---
+++ DsaQuestions/BinarySearch.java
@@ -0,0 +1,34 @@
+package DsaQuestions;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] OrrArr = {1,2,3,4,5,6,7,8,9,10,22,44,55,90};
+        int target = 5;
+        int res = search(OrrArr, target);
+        System.out.print(res);
+
+    }
+
+    //return the index
+    //return -1 if it does not exist
+
+    static int search(int[] arr, int target){
+        int start = 0;
+        int end = arr.length;
+
+        while(start < end){
+            int middle = start + (end - start) / 2;
+
+
+            if(target < arr[middle]){
+                end = middle - 1; // left shift
+            } else if (target > arr[middle]) {
+                start = middle + 1; // right shift
+            } else{
+                return middle;   // if target == middle
+            }
+        }
+        return -1; // not found
+
+    }
+}
