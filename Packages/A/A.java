2a3a401fa18d2bae56eac768c98e8ffaca4f3e1c
ummarFurquan <mohammedummar29@gmail.com>
2025-01-02 21:59:39 +0530

    Classes and Objects

---
+++ Packages/A/A.java
@@ -0,0 +1,11 @@
+package Packages.A;
+
+public class A {
+    public static void main(String[] args) {
+
+    }
+
+    public static void greeting(){
+        System.out.println("This is the class A package A ");
+    }
+}
